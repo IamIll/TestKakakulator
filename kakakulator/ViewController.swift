6981304f8b5fe4d08aac2ebd02a5238ac7b03c90
Ilay <idunaytsev@list.ru>
2022-04-05 15:59:25 +0300

    no message

---
+++ kakakulator/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  kakakulator
+//
+//  Created by Илья Дунайцев on 02.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var numberTextField: UITextField!
+    @IBAction func numberSeven(_ sender: UIButton) {
+    }
+    @IBAction func numberEight(_ sender: UIButton) {
+    }
+    @IBAction func numberNine(_ sender: UIButton) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ kakakulator/kakakulatorController.swift
@@ -0,0 +1,8 @@
+//
+//  kakakulatorController.swift
+//  kakakulator
+//
+//  Created by Илья Дунайцев on 02.04.2022.
+//
+
+import Foundation
